42bc207016201b1c935201f7df77c2a5ec2fd343
Shman4ik <shman4ik@gmail.com>
2024-09-23 23:10:20 +0200

    Update authentication and middleware in Program.cs
    Modified using directives to include necessary namespaces and remove redundant ones. Updated authentication services to set default schemes and configure cookies and Google authentication with secure options. Streamlined OAuthEvents for HTTPS redirects. Added middleware for forwarded headers to support reverse proxies. Maintained existing HSTS, authentication, and authorization middleware.

--- CampaignManager.Web/Program.cs
+++ CampaignManager.Web/Program.cs
@@ -4,11 +4,11 @@ using Blazorise.Tailwind;
 using CampaignManager.ServiceDefaults;
 using CampaignManager.Web.Components;
 using CampaignManager.Web.Services;
-using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Authentication.Cookies;
 using Microsoft.AspNetCore.Authentication.Google;
 using Microsoft.AspNetCore.Authentication.OAuth;
-using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.HttpOverrides;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -20,31 +20,31 @@ builder.Services.AddRazorComponents()
     .AddInteractiveServerComponents();
 
 builder.Services.AddAuthentication(options =>
+{
+    options.DefaultAuthenticateScheme = CookieAuthenticationDefaults.AuthenticationScheme;
+    options.DefaultSignInScheme = CookieAuthenticationDefaults.AuthenticationScheme;
+    options.DefaultChallengeScheme = GoogleDefaults.AuthenticationScheme;
+})
+.AddCookie()
+.AddGoogle(options =>
+{
+    options.ClientId = builder.Configuration["Authentication:Google:ClientId"];
+    options.ClientSecret = builder.Configuration["Authentication:Google:ClientSecret"];
+    options.CorrelationCookie.SecurePolicy = CookieSecurePolicy.Always;
+    options.CorrelationCookie.SameSite = SameSiteMode.None;
+    options.Events = new OAuthEvents
     {
-        options.DefaultAuthenticateScheme = CookieAuthenticationDefaults.AuthenticationScheme;
-        options.DefaultSignInScheme = CookieAuthenticationDefaults.AuthenticationScheme;
-        options.DefaultChallengeScheme = GoogleDefaults.AuthenticationScheme;
-    })
-    .AddCookie()
-    .AddGoogle(options =>
-    {
-        options.ClientId = builder.Configuration["Authentication:Google:ClientId"];
-        options.ClientSecret = builder.Configuration["Authentication:Google:ClientSecret"];        
-        options.CorrelationCookie.SecurePolicy = CookieSecurePolicy.Always;
-        options.CorrelationCookie.SameSite = SameSiteMode.None;
-        options.Events = new OAuthEvents
+        OnRedirectToAuthorizationEndpoint = context =>
         {
-            OnRedirectToAuthorizationEndpoint = context =>
+            if (context.RedirectUri.StartsWith("http:"))
             {
-                if (context.RedirectUri.StartsWith("http:"))
-                {
-                    context.RedirectUri = context.RedirectUri.Replace("http:", "https:");
-                }
-                context.Response.Redirect(context.RedirectUri);
-                return Task.CompletedTask;
+                context.RedirectUri = context.RedirectUri.Replace("http:", "https:");
             }
-        };
-    });
+            context.Response.Redirect(context.RedirectUri);
+            return Task.CompletedTask;
+        }
+    };
+});
 
 builder.Services.AddOutputCache();
 builder.Services
@@ -58,10 +58,14 @@ var app = builder.Build();
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Error", createScopeForErrors: true);
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
 
+// Add middleware for forwarded headers
+app.UseForwardedHeaders(new ForwardedHeadersOptions
+{
+    ForwardedHeaders = ForwardedHeaders.XForwardedFor | ForwardedHeaders.XForwardedProto
+});
 
 app.UseHttpsRedirection();
 
