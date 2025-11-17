f595d18894cd279a15c4213f108846c0393aeff1
Shman4ik <shman4ik@gmail.com>
2025-11-17 17:07:39 +0100

    fix styles

--- CampaignManager.Web/Components/Shared/Model/Eras.cs
+++ CampaignManager.Web/Components/Shared/Model/Eras.cs
@@ -5,7 +5,8 @@ namespace CampaignManager.Web.Components.Shared.Model;
 [Flags]
 public enum Eras
 {
-    [JsonStringEnumMemberName("1920x")] Classic = 1,
+    [JsonStringEnumMemberName("1920x")]
+    Classic = 1,
 
     [JsonStringEnumMemberName("Современность")]
     Modern = 2,
